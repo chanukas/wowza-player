81bddefdc7578c42453c8c8f918c8625185c0f17
Chanuka Sandaruwan <chanukaijse@gmail.com>
2018-05-14 17:09:12 +0530

    Add-homecontroller

---
+++ js/home-controller.js
@@ -0,0 +1,7 @@
+var myPlayer = WowzaPlayer.create('playerElement',{
+    "license":"PLAY1-ffcTE-nkGKD-bVmKQ-Aj4hj-twbcb",
+    "sourceURL":"http://128.199.111.221:1935/cima_vsp/Despacito1.mp4/playlist.m3u8",
+    "posterFrameURL":"http://128.199.111.221:8080/m.jpg",
+
+    }
+);
